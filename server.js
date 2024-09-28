0abf81af80b9d3e7f48bf136ba52d8f92e8a470a
KushagraTTiwari <kushagratiwari6162@gmail.com>
2024-09-28 20:33:03 +0530

    Second Version

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./db')
+require('dotenv').config();
 
 // const Person = require('./models/person')
 // const MenuItem = require('./models/MenuItem')
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     res.send('welcome to my hotel....How can i help you?')
@@ -23,7 +26,7 @@ app.use('/person', personRoutes)
 app.use('/menuitems', menuRoutes)
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('server is active')
     console.log('listening at 3000')
 });

886c8323d21498430a0c983433a4b19f7cb35ea7
KushagraTTiwari <kushagratiwari6162@gmail.com>
2024-11-10 17:59:01 +0530

    Completed

--- db.js
+++ db.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-//Define the MongoDB connection url
-// const mongoURL = 'mongodb://127.0.0.1:27017/hotels'
 const mongoURL = process.env.MONGODB_URL_GLOBAL
-console.log('this is url:- ',mongoURL)
-
-//Set up mongoDB connection
 mongoose.connect(mongoURL)
-//, {useNewUrlParser: true, useUnifiedTopology: true} no need to use these topology in new version.
 
 
-//Get the default connection
-//Mongoose maintains a default connection object representing the MongoDB connection.
 const db = mongoose.connection;
 
 db.on('connected', ()=> {
